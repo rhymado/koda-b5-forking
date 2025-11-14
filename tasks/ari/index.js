46f0e82f6b92f9bb21850c18b4a99869d607ced7
ariboss89 <ariboss89@gmail.com>
2025-11-14 23:22:57 +0700

    feat: add function import palindrom.js

--- tasks/ari/index.js
+++ tasks/ari/index.js
@@ -1,7 +1,9 @@
 import { romanToInteger } from "./romanToInteger.js";
+import { checkPalindrom } from "./palindrom.js";
 
 const func = {
   romanToInteger,
+  checkPalindrom,
 };
 
 export default func;
