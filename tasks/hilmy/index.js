c0e65bf2171495344135d61f0bd037617ece35b8
hilmy <haidaraly98@gmail.com>
2025-11-14 21:19:38 +0700

    feat: add kalkulator simpel

---
+++ tasks/hilmy/index.js
@@ -0,0 +1,22 @@
+function jumlah(a, b) {
+    return a + b;
+}
+
+function kali(a, b) {
+    return a * b;
+}
+
+function bagi(a, b) {
+    return a / b;
+}
+
+function kurang(a, b) {
+    return a - b;
+}
+
+jumlah(5, 10);
+kali(4, 3);
+bagi(20, 4);
+kurang(15, 5);
+
+
