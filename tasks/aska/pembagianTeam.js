04d42c7b42d0bfe39dc6fa45242fe06627cfb76c
aska diliya <askadiliyan@gmail.com>
2025-11-15 01:10:10 +0700

    fix[pembagianTeam]: fix feature pembagian

--- tasks/aska/pembagianTeam.js
+++ tasks/aska/pembagianTeam.js
@@ -1,23 +1,20 @@
 import { createInterface } from "node:readline/promises";
-// import { readline } from "node:readline/promises";
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+export async function bagiTeam() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-export async function bagiTeam(angka) {
-  if (angka % 2 === 0) {
-    return "Team B (genap)";
-  } else {
-    return "Team A (ganjil)";
-  }
-}
-
-rl.question("Masukkan angka pemain: ", (input) => {
+  const input = await rl.question("Masukkan angka pemain: ");
   const angka = Number(input);
 
-  console.log(bagiTeam(angka));
+  if (angka % 2 === 0) {
+    console.log("Team B (genap)");
+  } else {
+    console.log("Team A (ganjil)");
+  }
 
   rl.close();
-});
+}
+
