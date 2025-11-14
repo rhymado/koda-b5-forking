f5ac299aee88cadd5505c3ff0dee6c61d4043c20
kodacampmain <alden@kodacademy.id>
2025-11-14 19:35:18 +0700

    feat[fakhri]: run all fakhri function in both success and failure

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-import { rl, guessNumber } from "./src/guessNumber.js";
+/**
+ * Task Fakhri
+ */
 
-console.log("Halo Dunia");
-guessNumber(rl);
+import fakhri from "./tasks/fakhri/index.js";
+
+// Use IIFE here
+(function taskFakhri() {
+  // Convert Celcius to Fahrenheit
+  // Success
+  try {
+    const fahrenheit = fakhri.celciusToFahrenheit(10);
+    console.log(fahrenheit);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const fahrenheit = fakhri.celciusToFahrenheit("10", 30);
+    console.log(fahrenheit);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Counting Changes Nominal
+  // Success
+  try {
+    const changes = fakhri.cashier(250_000, 375_000);
+    console.log(changes);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const changes = fakhri.cashier("250_000", "375_000");
+    console.log(changes);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
+
+/**
+ * Task "Person Name"
+ */
+
+// import your tasks here and run
